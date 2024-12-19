8b2cb15ddbee4a57045a5cbd079f16bc852f8bf9
nathbarth <kolyenathalie@gmail.com>
2024-12-19 03:17:04 +0100

    Appliquer les règles du jeu

--- main.py
+++ main.py
@@ -22,7 +22,23 @@ def compute_number_neighbors(padded_frame, index_line, index_column):
 def compute_next_frame(frame):
     """Cette fonction prend en entrée une frame et calcule la frame suivante à partir des règles du jeu de la vie"""
 
-    paded_frame = np.pad(frame, 1, mode="constant") # je vous offre le code pour le zéro padding c'est cadeau !
+    padded_frame = np.pad(frame, 1, mode="constant")  # Ajout du padding
+    new_frame = np.copy(frame)  # Créer une copie de la matrice pour stocker la prochaine génération
+
+    for i in range(1, padded_frame.shape[0] - 1):  # Parcourir les cellules de la matrice avec padding
+        for j in range(1, padded_frame.shape[1] - 1):
+            # Calcul du nombre de voisins pour l'élément courant
+            neighbors = compute_number_neighbors(padded_frame, i, j)
+
+            # Appliquer les règles du jeu de la vie
+            if padded_frame[i, j] == 1:  # La cellule est vivante
+                if neighbors < 2 or neighbors > 3:
+                    new_frame[i - 1, j - 1] = 0  # La cellule meurt
+            elif padded_frame[i, j] == 0:  # La cellule est morte
+                if neighbors == 3:
+                    new_frame[i - 1, j - 1] = 1  # La cellule devient vivante
+
+    return new_frame
 
     """# Étape 1 : 2 boucles for imbriquées pour parcourir la matrice avec bordure (zero padding) element par element.Faites attention à l'index de début et d'arrêt ! (il s'agit de la matrice avec bordure)
 
@@ -36,7 +52,7 @@ def compute_next_frame(frame):
     Si c'est le cas effectuez les modifications directement dans la matrices frame (Attention à l'indice
     utilisé ! )"""
     
-    return frame
+    
 
 while True:
     # boucle infini qui affiche toutes les frames successives (ctrl + c pour arrêter le script)
